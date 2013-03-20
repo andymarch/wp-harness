949c40e894d182b70f12d79835de65f7b1776441
Andy March <mail@andymarch.co.uk>
2013-03-20 20:29:49 +0000

    Adding comments
    Fixing issue #2 adding the comments

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 	<div class="post">
 		<div class="PostHead"><span class="PostTitle"><a href="<?php the_permalink() ?>" title="<?php printf(__('Permalink to %s', 'sandbox'), wp_specialchars(get_the_title(), 1)) ?>" rel="bookmark"><?php the_title() ?></a></span>     <span class="PostDate"><?php the_time('jS F, Y'); ?></span></div>
 		<?php the_content(); ?>
-		<div class="PostFoot"></div>
+		<div class="PostFoot">
+			<?php the_tags(__('<span class="tag-links">Tagged ', 'sandbox'), ", ", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n") ?>
+			<?php edit_post_link(__('Edit', 'sandbox'), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n"); ?>
+			<span class="comments-link"><?php comments_popup_link(__('Comments (0)', 'sandbox'), __('Comments (1)', 'sandbox'), __('Comments (%)', 'sandbox')) ?></span>
+		</div>
 	</div>
+	<?php comments_template() ?>
 <?php endwhile;?>
 	<div id="nav-below" class="navigation">
 		<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'sandbox')) ?></div>

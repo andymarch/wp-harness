8ed12dcef26a4ea5c5068a7413184562adaf097e
Andy March <mail@andymarch.co.uk>
2016-01-16 13:12:38 +0000

    Updated site image swaped over to roboto font

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <title><?php bloginfo('name') ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Andy March">
-	<link href='http://fonts.googleapis.com/css?family=Lato:400,400italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 	<link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
     <link rel="shortcut icon" href="../assets/ico/favicon.ico">
 	<?php wp_head(); ?>
@@ -17,7 +17,7 @@
 		<div id="SiteLogo" class="container">
 			<p id="SiteName"><a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>" rel="home"><?php bloginfo('name') ?></a></p>
 			<p id="SiteDescription"><?php bloginfo('description') ?></p>
-			<p>
+			<p id="SocialMedia">
 			<a href="https://twitter.com/andymarch" title="Twitter" rel="home"><img class="socialLink" src="<?php bloginfo('template_url'); ?>/images/twitter.png"/></a>  
 			<a href="https://github.com/andymarch" title="GitHub" rel="home"><img class="socialLink" src="<?php bloginfo('template_url'); ?>/images/github.png"/></a>  
 			<a href="https://plus.google.com/115663453266966490755" title="Google+" rel="author"><img class="socialLink" src="<?php bloginfo('template_url'); ?>/images/google.png"/></a>  
